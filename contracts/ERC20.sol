5629a2b9f29bf99d57915568a5a98c6b7c660f8c
chenzx913 <583699803@qq.com>
2024-12-24 22:52:48 +0800

    second

---
+++ contracts/ERC20.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.27;
+
+import "./IERC20.sol";
+
+
+/**
+ * @dev ERC20 实现
+ */
+contract ERC20 is IERC20 {
+    // address 到 代币数量
+    mapping(address => uint256) public override balanceOf;
+
+    // address 到 授权 address 到 代币数量
+    mapping(address => mapping(address => uint256)) public override allowance;
+
+    // 代币总供给
+    uint256 public override totalSupply;   
+
+    string public name;   // 名称
+    string public symbol;  // 符号
+    
+    uint8 public decimals = 18; // 小数位数
+
+    // @dev 在合约部署的时候实现合约名称和符号
+    constructor(string memory name_, string memory symbol_){
+        name = name_;
+        symbol = symbol_;
+    }
+
+    // @dev 实现`transfer`函数，代币转账逻辑
+    function transfer(address recipient, uint amount) public override returns (bool) {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    // @dev 实现 `approve` 函数, 代币授权逻辑
+    function approve(address spender, uint amount) public override returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    // @dev 实现`transferFrom`函数，代币授权转账逻辑
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) public override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    // @dev 铸造代币，从 `0` 地址转账给 调用者地址
+    function mint(uint amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    // @dev 销毁代币，从 调用者地址 转账给  `0` 地址
+    function burn(uint amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+
+}
